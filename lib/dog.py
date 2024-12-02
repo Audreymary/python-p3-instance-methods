4bc889db5145ee945a9bfc0109a3806d938076e4
Audreymary <audreymoraa006@gmail.com>
2024-12-02 11:20:58 +0300

    add initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
-class Dog:
-    # Class body goes here
+# class Dog:
+#     # Class body goes here
 
-    #Instance method definition
-    pass
+#     #Instance method definition
+#     pass
+ 
+      
+class Dog:
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
+
+class Person:
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
+
